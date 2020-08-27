b6bb15b158fada4b7f96a6f2a3d4b2dc32a86b60
KKR-Spring <61870841+KKR-Spring@users.noreply.github.com>
2020-08-27 23:05:38 +0530

    Update KrApplicationTests.java
    Added comment for testing

--- src/test/java/com/littlehelpbykr/kr/KrApplicationTests.java
+++ src/test/java/com/littlehelpbykr/kr/KrApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KrApplicationTests {
-
+	// Adding Comment
 	@Test
 	public void contextLoads() {
 	}
